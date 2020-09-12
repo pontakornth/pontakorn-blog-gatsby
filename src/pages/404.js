d1e3413a9d595a5e344a4324e0f3b07284ddd0c0
pontakornth <pontakorn61@gmail.com>
2020-09-12 16:46:20 +0700

    feat: Add 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+
+const Page404 = () => (
+    <Layout>
+        <SEO />
+        <main style={{background: '#fff'}}>
+            <div class="wrapper">
+                <h1>404 Not Found</h1>
+                <span>Brrrrrr</span>
+            </div>
+        </main>
+    </Layout>
+)
+
+export default Page404
