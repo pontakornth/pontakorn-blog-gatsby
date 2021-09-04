08c6d30ede60c3a3d2391cf8cc45dbdef6ed81ec
pontakornth <pontakorn61@gmail.com>
2021-09-04 16:16:01 +0700

    feat: Make tag has hash in front of it

--- src/components/article-preview.js
+++ src/components/article-preview.js
@@ -21,7 +21,7 @@ export default ({ article }) => (
       {article.tags &&
         article.tags.map(tag => (
           <Link to={`/tags/${tag}`} className={styles.tag} key={tag}>
-            {tag}
+            #{tag}
           </Link>
         ))}
     </div>
